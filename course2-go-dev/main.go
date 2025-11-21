92aa01ef857232caeaa1947b25f2dfb2b272b936
fjkang <fjkang1991@gmail.com>
2025-11-21 10:56:40 +0800

    课程2代码迁移进course2-go-dev文件夹内

--- course2-go-dev/main.go
+++ course2-go-dev/main.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	// 1.创建4g大小的文件
 	upFilePath := "4gtestfile"
-	size := int64(4 * 1024 * 1024 * 1024)
-	// size := int64(40 * 1024 * 1024) //测试用
+	// size := int64(4 * 1024 * 1024 * 1024)
+	size := int64(40 * 1024 * 1024) //测试用
 	homework2.CreateNullFile(upFilePath, size)
 	fmt.Println("创建文件成功,文件名为：", upFilePath)
 	// 2.上传文件
-	fragmentSize := int64(400 * 1024 * 1024)
-	// fragmentSize := int64(4 * 1024 * 1024)  //测试用
+	// fragmentSize := int64(400 * 1024 * 1024)
+	fragmentSize := int64(4 * 1024 * 1024) //测试用
 	txHashs, rootHashs, _ := homework2.Upload(upFilePath, fragmentSize)
 	fmt.Println("上传文件成功,txHashs:", txHashs, "rootHashs:", rootHashs)
 	// 3.下载文件
